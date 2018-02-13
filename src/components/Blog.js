6367da7cf7d4548e957f8ccd51bf7dbb2e066cb3
Mikko Loponen <mikko.loponen@outotec.com>
2018-02-13 11:57:29 +0200

    5.6 detailit klikkaamalla

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,47 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: props.blog.title,
+      author: props.blog.author,
+      url: props.blog.url,
+      likes: props.blog.likes,
+      user: props.blog.user,
+      showDetails: false
+    }
+  }
+
+  toggleDetails = () => {
+    this.setState({ showDetails: !this.state.showDetails })
+  }
+
+  render() {
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    return (
+      <div
+        style={blogStyle}
+        onClick={this.toggleDetails}>
+
+        {this.state.title} ({this.state.author})
+        
+        {this.state.showDetails &&
+          <div style={{marginLeft:'10px'}}>
+            <a href={this.state.url}>{this.state.url}</a><br />
+            {this.state.likes} like(s)<br/>
+            {this.state.user && `added by ${this.state.user.name}`}
+        </div>}
+
+      </div>
+    )
+  }
+}
 
 export default Blog
